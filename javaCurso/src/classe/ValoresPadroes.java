784abc39fd7604fac1d7c5a004269b76f3fd76e2
Hydrasamy <silviosamysilva@hotmail.com>
2024-08-15 14:55:31 -0300

    Add files via upload

---
+++ javaCurso/src/classe/ValoresPadroes.java
@@ -0,0 +1,13 @@
+package classe;
+
+public class ValoresPadroes {
+	public static void main(String[] args) {
+			}
+}
+// byte, short , int, longe -> 0
+// float, double -> 0.0
+// boolean -> False
+// char -> '\u0000' Tabela UniCode.
+// Ele é dado quando não é definido nada na variável.
+// Objetos por padrão tem valor NULL (nulo).
+// String tem o valor nulo se não for inserido nada (ou seja, não aponta para nada)
