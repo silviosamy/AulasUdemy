7e937d64d509c5ea37896a5401b53fc15c83fda7
Silvio <silviosamysilva@hotmail.com>
2024-08-22 17:05:21 -0300

    Add files via upload

--- javaCurso/src/classe/ValorNulo.java
+++ javaCurso/src/classe/ValorNulo.java
@@ -7,12 +7,12 @@ public class ValorNulo {
 
 		System.out.println(s1.concat("!!!!"));
 		
-		Data d1 = null; //Não é possível acessar nem atributo, nem método de algo null.
+//		Data d1 = null; //Não é possível acessar nem atributo, nem método de algo null.
 //		d1.mes = 3; //A variável é um espaço de memória, podendo conter : Valor ou Endereço de memória
 //		             que aponta para um endereço onde o objeto foi criado
 //		Nenhuma expresão será verdadeira, se o valor for null.
-		if(d1 != null)
-			d1.mes = 3;
+//		if(d1 != null)
+//			d1.mes = 3;
 		
 //		String s2 = null;
 
