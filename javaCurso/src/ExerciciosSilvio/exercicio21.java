d66afaf7589c37a59791fb5d9ff99d3ac9ed0486
Bowie1 <marketing@letscode.com.br>
2024-09-02 16:50:43 -0300

    adição de exercícios feitos na internet

---
+++ javaCurso/src/ExerciciosSilvio/exercicio21.java
@@ -0,0 +1,7 @@
+package ExerciciosSilvio;
+
+public class exercicio21 {
+    public static void main(String[] args) {
+//
+    }
+}
---
+++ javaCurso/src/orientacaoObjetos/desafio/Item.java
@@ -0,0 +1,12 @@
+package orientacaoObjetos.desafio;
+
+public class Item {
+
+	final Produto produto;
+	final int quantidade;
+	
+	Item(Produto produto, int quantidade) {
+		this.produto = produto;
+		this.quantidade = quantidade;
+	}
+}
