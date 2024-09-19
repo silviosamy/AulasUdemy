79425d0416efaf9e83a07689428f267ffceb90a0
Bowie1 <marketing@letscode.com.br>
2024-09-19 15:40:11 -0300

    novos aprendizados e exercícios 19/09

--- javaCurso/src/streams/Map.java
+++ javaCurso/src/streams/Map.java
@@ -6,16 +6,16 @@ import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
 public class Map {
-    public static void main(String[] args) {
-
-        Consumer<String> print = System.out::print;
-
-        List<String> marcas = Arrays.asList("BMW ", "Audi", " Honda");
-//        Como aplicar o Map em Array:
-        marcas.stream().map(m -> m.toUpperCase()).forEach(print);
-//        Buildar stream, mapear a Array, colocar tudo em maiúscula, e printar.
-
-        UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+//    public static void main(String[] args) {
+//
+//        Consumer<String> print = System.out::print;
+//
+//        List<String> marcas = Arrays.asList("BMW ", "Audi", " Honda");
+////        Como aplicar o Map em Array:
+//        marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+////        Buildar stream, mapear a Array, colocar tudo em maiúscula, e printar.
+//
+//        UnaryOperator<String> maiuscula = n -> n.toUpperCase();
 
     }
 }
