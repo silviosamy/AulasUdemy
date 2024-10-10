c8e735d038a8e5bcefaaa6da05a706c6a543ee82
Bowie1 <marketing@letscode.com.br>
2024-10-10 17:11:59 -0300

    Aprendizados 10/10

---
+++ javaCurso/src/TreinoMetodos/UtilClass.java
@@ -0,0 +1,21 @@
+package TreinoMetodos;
+
+public class UtilClass {
+
+    public Pessoa novaPessoa(String nome, int idade) {
+        return new Pessoa(nome, idade);
+    }
+
+    public Carro novoCarro(String nome, int ano) {
+        return new Carro(nome, ano);
+    }
+
+    public ehVelho() {
+        if( < 2010)
+        {
+            return ehVelho = true;
+        } else{
+            return ehVelho = false;
+        }
+    }
+}
