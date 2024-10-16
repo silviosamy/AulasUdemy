5f971bdcc7939b2cc37185adf955c69cd1851c09
Bowie1 <marketing@letscode.com.br>
2024-10-16 17:19:55 -0300

    Aprendizados 16/10

---
+++ javaCurso/src/TreinoMedia/Aluno.java
@@ -0,0 +1,39 @@
+package TreinoMedia;
+
+public class Aluno {
+
+    private double n1;
+    private double n2;
+    private double n3;
+    private double n4;
+    private String nome;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private boolean amigoDoProfessor;
+
+    public Aluno(String nome, double n1, double n2, double n3, double n4, boolean amigoDoProfessor) {
+        this.nome = nome;
+        this.n1 = n1;
+        this.n2 = n2;
+        this.n3 = n3;
+        this.n4 = n4;
+        this.amigoDoProfessor = amigoDoProfessor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
---
+++ javaCurso/src/TreinoMedia/UtilClass.java
@@ -0,0 +1,19 @@
+package TreinoMedia;
+
+public class UtilClass {
+
+    private double mediaNota;
+
+    public double mediaNota(double n1, double n2, double n3, double n4) {
+        double mediaNotas = n1 + n2 + n3 + n4 / 4;
+        return mediaNota;
+    }
+
+    public double getMediaNota() {
+        return mediaNota;
+    }
+
+    public void setMediaNota(double mediaNota) {
+        this.mediaNota = mediaNota;
+    }
+}
