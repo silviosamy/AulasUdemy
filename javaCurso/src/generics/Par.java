b55792cca6df60a8379eaa51d97a0dda68256082
Bowie1 <marketing@letscode.com.br>
2024-11-28 17:10:44 -0300

    aprendizados generics 28/11 - Refatorar Class Pares conforme o curso

---
+++ javaCurso/src/generics/Par.java
@@ -0,0 +1,37 @@
+package generics;
+
+import java.util.Objects;
+
+public class Par<C, V> {
+
+    private C chave;
+    private V valor;
+
+
+    public Par(C chave, V valor) {
+        super();
+        this.chave = chave;
+        this.valor = valor;
+    }
+
+    public C getChave() {
+        return chave;
+    }
+
+    public void setChave(C chave) {
+        this.chave = chave;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Par<?, ?> par = (Par<?, ?>) o;
+        return Objects.equals(chave, par.chave) && Objects.equals(valor, par.valor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chave, valor);
+    }
+}
