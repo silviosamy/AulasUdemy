687d8522b8b4d90b62e9d0c23b1c80cd4ad51eb8
Silvio <silviosamysilva@hotmail.com>
2024-12-10 16:44:30 -0300

    Add files via upload
    Signed-off-by: Silvio <silviosamysilva@hotmail.com>

---
+++ SQL/inserirEmpresas.sql
@@ -0,0 +1,9 @@
+alter table empresas modify cnpj varchar(14);
+
+Insert into empresas (nome,cnpj)
+
+values
+	('Bradesco', 78947587648512),
+    ('Vale',15349267485247),
+    ('Cielo',49356874261859)
+
