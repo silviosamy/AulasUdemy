f17d05e40865c50603f179223ba723f10e0cb4b3
Silvio <silviosamysilva@hotmail.com>
2024-12-11 15:59:45 -0300

    Add files via upload
    Signed-off-by: Silvio <silviosamysilva@hotmail.com>

--- SQL/inserirEmpresas.sql
+++ SQL/inserirEmpresas.sql
@@ -1,9 +1,22 @@
-alter table empresas modify cnpj varchar(14);
+ALTER TABLE empresas MODIFY cnpj VARCHAR(14);
 
-Insert into empresas (nome,cnpj)
-
-values
+INSERT INTO empresas 
+	(nome,cnpj)
+VALUES
 	('Bradesco', 78947587648512),
     ('Vale',15349267485247),
     ('Cielo',49356874261859)
 
+desc empresas
+desc prefeitos
+
+select * from empresas;
+select * from cidades;
+
+INSERT INTO empresas_unidades
+	(empresa_id, cidade_id, sede)
+VALUES
+	(1, 1, 1),
+	(1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 2);
