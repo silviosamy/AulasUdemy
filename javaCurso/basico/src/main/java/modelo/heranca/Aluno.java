2c8cd31f7dd44357608ff595109b1e3c271f22c2
Silvio <silviosamysilva@hotmail.com>
2025-02-12 17:15:18 -0300

    Add files via upload
    Signed-off-by: Silvio <silviosamysilva@hotmail.com>

---
+++ javaCurso/basico/src/main/java/modelo/heranca/Aluno.java
@@ -0,0 +1,13 @@
+package modelo.heranca;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    private Long matricula;
+    
+
+}
---
+++ javaCurso/basico/src/main/java/modelo/heranca/AlunoBolsista.java
@@ -0,0 +1,4 @@
+package modelo.heranca;
+
+public class AlunoBolsista {
+}
