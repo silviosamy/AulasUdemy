391a45f7466a191cf9a9b7c68cbec500bad7360d
Silvio <silviosamysilva@hotmail.com>
2025-03-14 16:12:31 -0300

    Aprendizado API Rest 14/03

---
+++ src/test/java/com/example/api_java_spring_boot/ApiJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
